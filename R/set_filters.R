0e0d545591a7701ef7aa3369cbeaa4e85f06f38c
Mike Smith <grimbough@gmail.com>
2018-02-01 14:45:22 +0100

    Working versions of LZF and LZ4

--- R/set_filters.R
+++ R/set_filters.R
@@ -1,14 +1,24 @@
 
-## LZF Filter
+#' LZF Filter
+#' @export
 H5Pset_lzf <- function( h5plist ) {
+    
+    if(!is.loaded('_H5Pset_lzf', PACKAGE = 'rhdf5'))
+        stop('LZF filter not found.  Please reinstall rhdf5.')
+    
     rhdf5:::h5checktypeAndPLC(h5plist, "H5P_DATASET_CREATE")
-    res <- .Call("_H5Pset_lzf", h5plist@ID, PACKAGE='hdf5Filters')
+    res <- .Call("_H5Pset_lzf", h5plist@ID, PACKAGE='rhdf5')
     invisible(res)
 }
 
 ## LZ4 Filter
-#H5Pset_lz4 <- function( h5plist ) {
-#    h5checktypeAndPLC(h5plist, "H5P_DATASET_CREATE")
-#    res <- .Call("_H5Pset_lz4", h5plist@ID, PACKAGE='hdf5Filters')
-#    invisible(res)
-#}
+#' @export
+H5Pset_lz4 <- function( h5plist ) {
+    
+    if(!is.loaded('_H5Pset_lz4', PACKAGE = 'rhdf5'))
+        stop('LZ4 filter not found.  Please reinstall rhdf5.')
+    
+    rhdf5:::h5checktypeAndPLC(h5plist, "H5P_DATASET_CREATE")
+    res <- .Call("_H5Pset_lz4", h5plist@ID, PACKAGE='rhdf5')
+    invisible(res)
+}
--- R/zzz.R
+++ R/zzz.R
@@ -16,3 +16,42 @@
     
     Sys.setenv(HDF5_PLUGIN_PATH=patharch)
 }
+
+#' @export
+pkgconfig <- function(opt = c("PKG_LIB")) {
+    
+    path <- Sys.getenv(
+        x = "HDF5FILTERS_RPATH",
+        unset = system.file("lib", package="hdf5Filters", mustWork=TRUE)
+    )
+    
+    if (nzchar(.Platform$r_arch)) {
+        arch <- sprintf("/%s", .Platform$r_arch)
+    } else {
+        arch <- ""
+    }
+    patharch <- paste0(path, arch)
+    
+    result <- switch(match.arg(opt), 
+                     PKG_LIB = {
+                         switch(Sys.info()['sysname'], 
+                                Linux = {
+                                    sprintf('-Wl,-rpath=%s -L%s -llzf_filter',
+                                            patharch, patharch)
+                                #}, Darwin = {
+                                #     sprintf('%s/libhdf5.a %s/libsz.a', 
+                                #             patharch, patharch)
+                                # }, Windows = {
+                                #     patharch <- gsub(x = shortPathName(patharch),
+                                #                      pattern = "\\",
+                                #                      replacement = "/", 
+                                #                      fixed = TRUE)
+                                #     sprintf('-L%s -lhdf5 -lszip -lz -lpsapi', 
+                                #             patharch)
+                                }
+                         )
+                     } 
+    )
+    
+    cat(result)
+}
