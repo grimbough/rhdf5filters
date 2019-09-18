b4acb7e8027b58dad8d53a7c1840a6e0c666a0ad
Mike Smith <grimbough@gmail.com>
2019-09-18 09:21:51 +0200

    Updated windows build to include lzf

--- src/winlib/build-commands.sh
+++ src/winlib/build-commands.sh
@@ -1,21 +1,6 @@
-cd src/blosc/lib/lz4-r119
-${CC} ${CFLAGS} -fPIC -c *.c
-cd ../snappy-1.1.1
-${CXX} ${CXXFLAGS} -fPIC -c *.cc
-cd ../blosc-1.4.1
-${CC} ${CFLAGS} -fPIC -I../lz4-r119/ -I../snappy-1.1.1/ -c *.c
-cd ../../
-${CC} ${CFLAGS} -fPIC -I${RHDF5_INCLUDE} -I./lib/blosc-1.4.1/ -c H5Zblosc.c
-## Linking step
-${CXX} ${CXXFLAGS} -fPIC -shared \
-    -I${RHDF5_INCLUDE} -I./lib/blosc-1.4.1/ \
-    -o libh5blosc.so \
-    H5Zblosc.o lib/blosc-1.4.1/*.o lib/lz4-r119/*.o lib/snappy-1.1.1/*.o \
-    ${RHDF5_LIBS} -lz -lpthread
-cd ../../
 
 CC=/c/Rtools/mingw_32/bin/gcc.exe
-CXX=/c/Rtools/mingw_32/bin/gcc.exe
+CXX=/c/Rtools/mingw_32/bin/g++.exe
 RHDF5_INCLUDE=/c/Users/Mike/DOCUME~1/R/WIN-LI~1/3.4/Rhdf5lib/include/
 RHDF5_LIBS="-L/c/Users/Mike/Documents/R/win-library/3.4/Rhdf5lib/lib/i386/"
 
@@ -37,7 +22,7 @@ mv libh5blosc.dll ../winlib/i386/
 	
 	
 CC=/c/Rtools/mingw_64/bin/gcc.exe
-CXX=/c/Rtools/mingw_64/bin/gcc.exe
+CXX=/c/Rtools/mingw_64/bin/g++.exe
 RHDF5_INCLUDE=/c/Users/Mike/DOCUME~1/R/WIN-LI~1/3.4/Rhdf5lib/include/
 RHDF5_LIBS="-L/c/Users/Mike/Documents/R/win-library/3.4/Rhdf5lib/lib/x64/"
 
@@ -55,3 +40,28 @@ ${CXX} -shared \
     H5Zblosc.o lib/blosc-1.4.1/*.o lib/lz4-r119/*.o lib/snappy-1.1.1/*.o \
     ${RHDF5_LIBS} -lhdf5 -lszip -lz -lstdc++
 mv libh5blosc.dll ../winlib/x64/
+
+
+
+
+CC=/c/Rtools/mingw_32/bin/gcc.exe
+RHDF5_INCLUDE="$HOME/DOCUME~1/R/WIN-LI~1/3.4/Rhdf5lib/include/"
+RHDF5_LIBS="-L$HOME/Documents/R/win-library/3.4/Rhdf5lib/lib/i386/"
+
+cd "$HOME/Code/github/hdf5Filters/src/lzf"
+${CC} -I"${RHDF5_INCLUDE}" "${RHDF5_LIBS}" -shared lib/*.c lzf_filter.c \
+  "$HOME/Code/bioconductor/Rhdf5lib/src/winlib/i386/hdf5.dll" \
+  "$HOME/Code/bioconductor/Rhdf5lib/src/winlib/i386/szip.dll" -lz -o liblzf_filter.dll
+mv liblzf_filter.dll ../winlib/i386/
+
+
+
+CC=/c/Rtools/mingw_64/bin/gcc.exe
+RHDF5_INCLUDE="$HOME/DOCUME~1/R/WIN-LI~1/3.4/Rhdf5lib/include/"
+RHDF5_LIBS="-L$HOME/Documents/R/win-library/3.4/Rhdf5lib/lib/x64/"
+
+cd "$HOME/Code/github/hdf5Filters/src/lzf"
+${CC} -I"${RHDF5_INCLUDE}" "${RHDF5_LIBS}" -shared lib/*.c lzf_filter.c \
+  "$HOME/Code/bioconductor/Rhdf5lib/src/winlib/x64/hdf5.dll" \
+  "$HOME/Code/bioconductor/Rhdf5lib/src/winlib/x64/szip.dll" -lz -o liblzf_filter.dll
+mv liblzf_filter.dll ../winlib/x64/
