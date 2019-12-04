fad5ebe9b6301640ccc7a3334113018f04a2f50e
Mike Smith <grimbough@gmail.com>
2019-12-04 09:45:04 +0100

    add README

---
+++ README.md
@@ -0,0 +1,23 @@
+# rhdf5filters
+
+<img align = "right" src="https://raw.githubusercontent.com/Bioconductor/BiocStickers/master/rhdf5/rhdf5.png" height="200">
+
+This R package provides an R distribution of several compression filters that can be applied to HDF5 datasets.
+
+## Current Status
+
+| Travis        | Test Coverage |
+| ------------- | -----|
+| [![Build Status](https://travis-ci.org/grimbough/rhdf5.svg?branch=master)](https://travis-ci.org/grimbough/rhdf5filters) |  [![Codecov](http://img.shields.io/codecov/c/github/grimbough/rhdf5.svg)](https://codecov.io/gh/grimbough/rhdf5filters) |
+
+## Contact
+
+For bug reports, please register an [issue](https://github.com/grimbough/rhdf5filters/issues) here on Github. 
+
+## Funding 
+
+Funding for continued development and maintenance of this package is provided by the German Network for Bioinformatics Infrastructure & the Chan Zuckerberg Initiative.
+
+|    |    |
+|:---|---:|
+|<a href="http://www.denbi.de"><img src="https://tess.elixir-europe.org/system/content_providers/images/000/000/063/original/deNBI_Logo_rgb.jpg" width="400" align="left"></a>|<img src="https://image4.owler.com/logo/chan-zuckerberg-initiative_owler_20160616_114930_original.png" width="257" align="right">|
--- tests/testthat/test_bzip2.R
+++ tests/testthat/test_bzip2.R
@@ -9,16 +9,16 @@ if(requireNamespace("rhdf5")) {
     ############################################################
     
     fid <- H5Fcreate(h5File)
-    sid <- H5Screate_simple(dims = 20, maxdims = 20)
+    sid <- H5Screate_simple(dims = 200000, maxdims = 200000)
     tid <- rhdf5:::.setDataType(H5type = NULL, storage.mode = "integer")
     
     test_that("BZIP2 filter can be used for writing", {
         expect_silent( dcpl <- H5Pcreate("H5P_DATASET_CREATE") )
         expect_silent( H5Pset_fill_time( dcpl, "H5D_FILL_TIME_ALLOC" ) )
-        expect_silent( H5Pset_chunk( dcpl, 20) )
+        expect_silent( H5Pset_chunk( dcpl, 200000) )
         expect_silent( hdf5Filters::H5Pset_bzip2(dcpl) )
         expect_silent( did <- H5Dcreate(fid, "bzip2", tid, sid, dcpl = dcpl) )
-        expect_silent( H5Dwrite(buf = 1:20, h5dataset = did) )
+        expect_silent( H5Dwrite(buf = 1:200000, h5dataset = did) )
         expect_silent( H5Dclose(did) )
     })
     
@@ -32,9 +32,9 @@ if(requireNamespace("rhdf5")) {
     test_that("BZIP2 filter can be used for reading", { 
         expect_silent( fid <- H5Fopen(h5File) )
         expect_silent( did <- H5Dopen(fid, name = "bzip2") )
-        expect_equivalent( H5Dread(did), 1:20)
+        expect_equivalent( H5Dread(did), 1:200000)
         ## if compression worked the dataset should be smaller than 80 bytes
-        expect_lt( H5Dget_storage_size(did), 4 * 20 )
+        expect_lt( H5Dget_storage_size(did), 4 * 200000 )
         expect_silent( H5Dclose(did) )
         expect_silent( H5Fclose(fid) )
     })
