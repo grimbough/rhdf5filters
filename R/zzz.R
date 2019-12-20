4ef43af31bdac79020b597da77bb333caadd417f
Mike Smith <grimbough@gmail.com>
2019-12-20 14:13:45 +0100

    Add function to return plugin path

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,21 @@
 
-
 .onLoad <- function(libname, pkgname) {
     
-    path <- Sys.getenv(
-        x = "HDF5FILTERS_RPATH",
-        unset = system.file("lib", package="hdf5Filters", mustWork=FALSE)
-    )
+    patharch <- hdf5_plugin_path()
+    Sys.setenv(HDF5_PLUGIN_PATH=patharch)
+}
+
+#' Return location of installed filters
+#' 
+#' This function can be used to find the location of the installed filters.
+#' Setting the environment variable HDF5_PLUGIN_PATH to this value will allow
+#' other applications that require the the filters to use the versions
+#' distributed with this package.
+#' 
+#' @export
+hdf5_plugin_path <- function() {
+    
+    path <- system.file("lib", package="rhdf5filters", mustWork=FALSE)
     
     ## record the architecture if it exists
     if (nzchar(.Platform$r_arch)) {
@@ -15,44 +25,5 @@
     }
     patharch <- paste0(path, arch)
     
-    Sys.setenv(HDF5_PLUGIN_PATH=patharch)
-}
-
-#' @export
-pkgconfig <- function(opt = c("PKG_LIB")) {
-    
-    path <- Sys.getenv(
-        x = "HDF5FILTERS_RPATH",
-        unset = system.file("lib", package="hdf5Filters", mustWork=TRUE)
-    )
-    
-    if (nzchar(.Platform$r_arch)) {
-        arch <- sprintf("/%s", .Platform$r_arch)
-    } else {
-        arch <- ""
-    }
-    patharch <- paste0(path, arch)
-    
-    result <- switch(match.arg(opt), 
-                     PKG_LIB = {
-                         switch(Sys.info()['sysname'], 
-                                Linux = {
-                                    sprintf('-L%s -lh5blosc -lH5Zbz2',
-                                            patharch)
-                                #}, Darwin = {
-                                #     sprintf('%s/libhdf5.a %s/libsz.a', 
-                                #             patharch, patharch)
-                                # }, Windows = {
-                                #     patharch <- gsub(x = shortPathName(patharch),
-                                #                      pattern = "\\",
-                                #                      replacement = "/", 
-                                #                      fixed = TRUE)
-                                #     sprintf('-L%s -lhdf5 -lszip -lz -lpsapi', 
-                                #             patharch)
-                                }
-                         )
-                     } 
-    )
-    
-    cat(result)
+    return(patharch)
 }
