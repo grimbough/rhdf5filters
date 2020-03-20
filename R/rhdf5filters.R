68adcd40e024e1190166aef9de632f6fc247ca9f
Mike Smith <grimbough@gmail.com>
2020-03-20 16:38:47 +0100

    Some change to man pages

--- R/rhdf5filters.R
+++ R/rhdf5filters.R
@@ -1,6 +1,7 @@
 #' rhdf5filters
 #' 
-#' This package provides compiled versions of several HDF5 compression filters
+#' This package provides several HDF5 compression filters for use with rhdf5 
+#' or other tools using HDF5.
 #'
 #' @docType package
 #' @name rhdf5filters
