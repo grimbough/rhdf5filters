d1d08ff1048884954aa5ec3ccd7c8824aa5abf21
Mike Smith <grimbough@gmail.com>
2023-07-17 13:41:04 +0200

    Address warning -Wexpansion-to-defined warning in lzf

--- src/lzf/lzf/lzfP.h
+++ src/lzf/lzf/lzfP.h
@@ -79,7 +79,12 @@
  * Unconditionally aligning does not cost very much, so do it if unsure
  */
 #ifndef STRICT_ALIGN
-# define STRICT_ALIGN !(defined(__i386) || defined (__amd64))
+/* Modified in response to clang warning -Wexpansion-to-defined */
+//# define STRICT_ALIGN !(defined(__i386) || defined (__amd64))
+#if defined(__i386) || defined (__amd64)
+# define STRICT_ALIGN 0
+#else 
+# define STRICT_ALIGN 1
 #endif
 
 /*
